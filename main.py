5a36fc15fd9437e3257bbf81d25f21c902b407b8
catcodey <103652349+catcodey@users.noreply.github.com>
2026-03-13 21:48:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from google.oauth2 import service_account
 
 # --- 1. CONFIGURATION ---
 PROJECT_ID = "transcript-summarizer-490013"
-LOCATION = "us-central1"
+LOCATION = "us-east4"
 
 # Download necessary NLTK data
 try:
